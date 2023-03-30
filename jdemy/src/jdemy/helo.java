332d905d8646bd94f174a3e253efea254202b5c7
sathi <sathi@DESKTOP-74OG3LE>
2023-03-30 22:32:42 +0530

    wewqe

--- jdemy/src/jdemy/helo.java
+++ jdemy/src/jdemy/helo.java
@@ -2,8 +2,8 @@ package jdemy;
 
 public class helo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub          
 System.out.println("iam back bby");
 	}
 
